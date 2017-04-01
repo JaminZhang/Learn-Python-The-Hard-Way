eed577ac1f1c5cff20edb58ba4b2fb13b4c9a9fd
JaminZhang <zhangjamin@163.com>
2017-04-01 10:45:42 +0800

    Create ex1-studydrills.py

---
+++ Python2/ex1-studydrills.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#print "Hello World!"
+#print "Hello Aagin"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print "This is another line."
+print "One line to rule them all."
