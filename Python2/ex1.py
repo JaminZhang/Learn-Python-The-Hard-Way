536efd13873b30bb00f7af1d38e0d220c852edb0
JaminZhang <zhangjamin@163.com>
2017-04-01 10:44:07 +0800

    Create ex1.py

---
+++ Python2/ex1.py
@@ -0,0 +1,9 @@
+# ex1: A Good First Program
+
+print "Hello World!"
+print "Hello Aagin"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
