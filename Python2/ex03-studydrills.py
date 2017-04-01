22fa6d170dbcb4feca9a7e92e8ddc890acfd2a57
JaminZhang <zhangjamin@163.com>
2017-04-01 22:36:34 +0800

    Create ex03-studydrills.py

---
+++ Python2/ex03-studydrills.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# ex03: Numbers and Math
+
+# Print "I will now count my chickens:"
+print "I will now count my chickens:"
+
+# Print the number of hens: 25 + (30.0 / 6)
+print "Hens", 25 + 30.0 / 6
+# Print the number of roosters: 100 - ((25 * 3) % 4)
+print "Roosters", 100 - 25 * 3 % 4
+
+# Print "Now I will count the eggs:"
+print "Now I will count the eggs:"
+
+# Number of eggs, Notice that '/' operator returns int value in Python2.
+# Number of eggs: 3 + 2 + 1 - 5 + (4 % 2) - (1.0 / 4) + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6 # Use floating point numbers so it's more accurate.
+
+# Print "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Print whether 3 + 2 is less than 5 - 7 (True or False): (3 + 2) < (5 - 7)
+print 3 + 2 < 5 - 7
+
+# Calculate 3 + 2 and print the result
+print "What is 3 + 2?", 3 + 2
+# Calculate 5 - 7 and print the result
+print "What is 5 - 7?", 5 - 7
+
+# Print "Oh, that's why it's False."
+print "Oh, that's why it's False."
+
+# Print "How about some more."
+print "How about some more."
+
+# Print whether 5 is greater than -2 (True or False)
+print "Is it greater?", 5 > -2
+# Print whether 5 is greater than or equal to -2 (True or False)
+print "Is it greater or equal?", 5 >= -2
+# Print whether 5 is less than or equal to -2 (True or False)
+print "Is it less or equal?", 5 <= -2
+
+print "In amazon.com, Apple Macbook Pro 2016 15.4-inch Laptop with Touch Bar(2.7GHz quad-core Intel Core i7, 512GB Retina Display), Space Gray is", 2720 * 6.8831, "RMB"
+print "In jd.com, it's 21488 RMB."
