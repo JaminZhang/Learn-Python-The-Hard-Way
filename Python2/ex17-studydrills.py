09a01c64d5ec052875086b3aa930445646b36c1a
JaminZhang <zhangjamin@163.com>
2017-04-04 19:56:41 +0800

    Update ex17-studydrills.py

--- Python2/ex17-studydrills.py
+++ Python2/ex17-studydrills.py
@@ -7,6 +7,6 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-# Make the core part one line log.
+# Make the core part one line long.
 open(to_file, 'w').write(open(from_file).read())
 #open(argv[2], 'w').write(open(argv[1]).read())
